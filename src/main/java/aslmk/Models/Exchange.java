6ac4061f46b64116fd2f52c55470e4b156c012b0
aslmk <aulaensov@gmail.com>
2024-09-13 23:42:53 +0500

    Rewrite Exchange and ExchangeRate models.

--- src/main/java/aslmk/Models/Exchange.java
+++ src/main/java/aslmk/Models/Exchange.java
@@ -1,20 +1,4 @@
 package aslmk.Models;
 
-import aslmk.Models.Currency;
-
-public class Exchange {
-    private Currency baseCurrency;
-    private Currency targetCurrency;
-    private double rate;
-    private double amount;
-    private double convertedAmount;
-
-    public Exchange(Currency baseCurrency, Currency targetCurrency,
-                    double rate, double amount, double convertedAmount) {
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-        this.amount = amount;
-        this.convertedAmount = convertedAmount;
-    }
-}
+public record Exchange(Currency baseCurrency, Currency targetCurrency,
+        double rate, double amount, double convertedAmount) {}
--- src/main/java/aslmk/Models/ExchangeRate.java
+++ src/main/java/aslmk/Models/ExchangeRate.java
@@ -1,17 +1,3 @@
 package aslmk.Models;
 
-import aslmk.Models.Currency;
-
-public class ExchangeRate {
-    private int id;
-    private Currency baseCurrency;
-    private Currency targetCurrency;
-    private double rate;
-
-    public ExchangeRate(int id, Currency baseCurrency, Currency targetCurrency, double rate) {
-        this.id = id;
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-    }
-}
+public record ExchangeRate(int id, Currency baseCurrency, Currency targetCurrency, double rate) {}

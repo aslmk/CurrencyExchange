7d5b95c6cbbe609af84e3c649de38a25dd69754a
aslmk <aulaensov@gmail.com>
2024-09-21 16:41:58 +0500

    Refactor isCurrencyCode method

--- src/main/java/aslmk/Utils/ValidationUtil.java
+++ src/main/java/aslmk/Utils/ValidationUtil.java
@@ -3,5 +3,6 @@ package aslmk.Utils;
 public class ValidationUtil {
     public static boolean isCurrencyCode(String code) {
         return code != null && code.length() == 3 && code.matches("[A-Z]{3}");
+
     }
 }

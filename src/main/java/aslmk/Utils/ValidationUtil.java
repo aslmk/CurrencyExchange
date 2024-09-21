119bf1a966bc7c87fe69b83cffe95854366f2a54
aslmk <aulaensov@gmail.com>
2024-09-21 21:15:45 +0500

    Add isParameters() method in ValidationUtil class

--- src/main/java/aslmk/Utils/ValidationUtil.java
+++ src/main/java/aslmk/Utils/ValidationUtil.java
@@ -3,6 +3,10 @@ package aslmk.Utils;
 public class ValidationUtil {
     public static boolean isCurrencyCode(String code) {
         return code != null && code.length() == 3 && code.matches("[A-Z]{3}");
-
+    }
+    public static boolean isParameters(String currencyFullName, String currencyCode, String currencySign) {
+        return (currencyFullName != null && !currencyFullName.equals("")) &&
+                (currencyCode != null && !currencyCode.equals("")) &&
+                (currencySign != null && !currencySign.equals(""));
     }
 }

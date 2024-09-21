58dc2370188e5e22aaef9a68216050a4559bb570
aslmk <aulaensov@gmail.com>
2024-09-21 17:53:40 +0500

    Add setResponse() method in Utils class

--- src/main/java/aslmk/Utils/Utils.java
+++ src/main/java/aslmk/Utils/Utils.java
@@ -1,14 +1,21 @@
 package aslmk.Utils;
 
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
 public class Utils {
     public static String getCurrencyCodeFromURL(String url) {
-        if (url == null || url.equals("/")) {
-            return "";
+        if (url == null || url.isEmpty()) {
+            return null;
         }
         String currencyUrl = url.substring(1);
 
-        if (currencyUrl.contains("/")) {
-            return "";
+        if (currencyUrl.isEmpty() || currencyUrl.contains("/")) {
+            return null;
         }
         return currencyUrl;
 
@@ -27,4 +34,11 @@ public class Utils {
 
     }
 
+    public static void setResponse(HttpServletResponse resp, Object object) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter();
+        Gson gson = new Gson();
+        pw.write(gson.toJson(object));
+    }
 }

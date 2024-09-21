d91077faa83f37d59ee304484a6e859d5eab00d9
aslmk <aulaensov@gmail.com>
2024-09-21 15:26:09 +0500

    Refactor currencyNotFoundException method in ResponseHandlingUtil class

--- src/main/java/aslmk/Utils/ResponseHandlingUtil.java
+++ src/main/java/aslmk/Utils/ResponseHandlingUtil.java
@@ -2,13 +2,20 @@ package aslmk.Utils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResponseHandlingUtil {
-    public void dbException(HttpServletResponse resp) throws IOException {
+    public static void dbException(HttpServletResponse resp) throws IOException {
         resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Something wrong with database!");
     }
-    public void currencyNotFoundException(HttpServletResponse resp) throws IOException {
-        resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Currency not found!");
+    public static void currencyNotFoundException(HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter pw = resp.getWriter();
+        pw.write("{\"message\": \"Currency not found!\"}");
+        pw.flush();
     }
     public void notEnoughParameters(HttpServletResponse resp) throws IOException {
         resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Not enough parameters!");
@@ -16,4 +23,5 @@ public class ResponseHandlingUtil {
     public void alreadyExists(HttpServletResponse resp) throws IOException {
         resp.sendError(HttpServletResponse.SC_CONFLICT, "Currency already exists!");
     }
+
 }

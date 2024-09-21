5864bac03517cff03453ee9fbb5f9de0f7b500b9
aslmk <aulaensov@gmail.com>
2024-09-21 16:01:00 +0500

    Refactor ResponseHandlingUtil class

--- src/main/java/aslmk/Utils/ResponseHandlingUtil.java
+++ src/main/java/aslmk/Utils/ResponseHandlingUtil.java
@@ -5,23 +5,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ResponseHandlingUtil {
-    public static void dbException(HttpServletResponse resp) throws IOException {
-        resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Something wrong with database!");
-    }
-    public static void currencyNotFoundException(HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+    public static void sendError(HttpServletResponse resp, int status, String message) throws IOException {
+        resp.setStatus(status);
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
         PrintWriter pw = resp.getWriter();
-        pw.write("{\"message\": \"Currency not found!\"}");
+        pw.write("{\"message\": \"" + message + "\"}");
         pw.flush();
     }
-    public void notEnoughParameters(HttpServletResponse resp) throws IOException {
-        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Not enough parameters!");
+
+    public static void dataBaseMessage(HttpServletResponse resp) throws IOException {
+        sendError(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Something wrong with database!");
     }
-    public void alreadyExists(HttpServletResponse resp) throws IOException {
-        resp.sendError(HttpServletResponse.SC_CONFLICT, "Currency already exists!");
+    public static void currencyNotFoundMessage(HttpServletResponse resp) throws IOException {
+        sendError(resp, HttpServletResponse.SC_NOT_FOUND, "Currency not found!");
+    }
+    public void notEnoughParametersMessage(HttpServletResponse resp) throws IOException {
+        sendError(resp, HttpServletResponse.SC_BAD_REQUEST, "Not enough parameters!");
+    }
+    public void alreadyExistsMessage(HttpServletResponse resp) throws IOException {
+        sendError(resp, HttpServletResponse.SC_CONFLICT, "Currency already exists!");
     }
 
 }

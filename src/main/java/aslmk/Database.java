bfad92daaf14c26611a106fd1849221ac61bf97d
aslmk <aulaensov@gmail.com>
2024-09-23 23:21:03 +0500

    Use singleton pattern in Database class

--- src/main/java/aslmk/Database.java
+++ src/main/java/aslmk/Database.java
@@ -10,16 +10,8 @@ import java.net.URL;
 
 public class Database {
     private Connection connection = null;
-    //private static Database instance = null;
-    //private Database() {}
-    /*public static Database getInstance() {
-        if (instance == null) {
-            instance = new Database();
-        }
-        return instance;
-    }*/
-
-    public boolean openConnection() {
+    private static Database instance = null;
+    private Database() {
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
@@ -32,19 +24,17 @@ public class Database {
             }
             String path = new File(resource.toURI()).getAbsolutePath();
             connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s", path));
-            return true;
         } catch (SQLException | FileNotFoundException | URISyntaxException e) {
             e.printStackTrace();
-            return false;
         }
     }
-    public void closeConnection() {
-        try {
-            if (connection != null) connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
         }
+        return instance;
     }
+
     public Connection getConnection() {
         return connection;
     }
